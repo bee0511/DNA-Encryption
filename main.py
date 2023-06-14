8bd9d3df01b977d2c95bf82d86eb006a109c79c9
bee0511 <bee000092@gmail.com>
2023-06-14 14:46:46 +0800

    change to main

--- main.py
+++ main.py
@@ -5,23 +5,20 @@ if __name__ == '__main__':
 
     EncrypOrDecrypt = input("Encrypt or decrypt a message?\nPress 1 for encryption, press 2 for decryption: ")
 
-    match EncrypOrDecrypt: 
-        case "1":
-            ans = input("What do you want to encrypt? \nPress 1 for std input, press 2 for file: ")
-            match ans:
-                case "1":
-                    DNA.TextEncryption()
-                case "2":
-                    file = input("Enter your file name. Ex: 'test.txt' or 'test.jpg' \n")
-                    DNA.FileEncryption(file)
-            print("You can find your cipher in 'Cipher.txt' and your key in 'Key.txt")
+    if EncrypOrDecrypt == "1": 
+        ans = input("What do you want to encrypt? \nPress 1 for std input, press 2 for file: ")
+        if ans == "1":
+                DNA.TextEncryption()
+        elif ans == "2":
+                file = input("Enter your file name. Ex: 'test.txt' or 'test.jpg' \n")
+                DNA.FileEncryption(file)
+        print("You can find your cipher in 'Cipher.txt' and your key in 'Key.txt")
 
-        case "2":
-            ans = input("What do you want to decrypt? \nPress 1 for .txt file, press 2 for img file: ")
-            match ans:
-                case "1":
-                    DNA.Decryption("Cipher.txt", "Key.txt", type="text")
-                    print("You can find your decoded message in 'PlainTextResult.txt'")
-                case "2":
-                    DNA.Decryption("Cipher.txt", "Key.txt", type="image")
-                    print("You can find your decoded image in 'DecodedResult.png'")
+    if EncrypOrDecrypt == "2":
+        ans = input("What do you want to decrypt? \nPress 1 for .txt file, press 2 for img file: ")
+        if ans == "1":
+            DNA.Decryption("Cipher.txt", "Key.txt", type="text")
+            print("You can find your decoded message in 'PlainTextResult.txt'")
+        elif ans == "2":
+            DNA.Decryption("Cipher.txt", "Key.txt", type="image")
+            print("You can find your decoded image in 'DecodedResult.png'")
